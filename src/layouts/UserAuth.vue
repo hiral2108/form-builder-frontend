0d4079b936eaf417a978c56e75ce560c0018d522
anjalisinojiya <anjali@klaxonsolutions.com>
2026-08-03 17:07:30 +0530

    create login and register file

---
+++ src/layouts/UserAuth.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="root">
+    <div class="min-h-screen bg-gray-50">
+      <div class="min-h-screen flex justify-center">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/types/auth.ts
@@ -0,0 +1,9 @@
+// export interface IAuth {
+//   access_token: string;
+//   message: string;
+// }
+
+export interface verifyToken {
+  verify_token: boolean;
+  message: string;
+}
